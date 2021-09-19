ead91845ff14e33a5d9212e8798957ec0efb2d0c
Mateusz <mateuszzygadlo00@gmail.com>
2021-09-20 00:38:30 +0200

    add slider

---
+++ main.js
@@ -0,0 +1,65 @@
+const nextSlide = document.querySelector('.next');
+const backSlide = document.querySelector('.back');
+const point = document.querySelector('.points');
+const points = document.querySelectorAll('.point');
+const slider = document.querySelector('.slider');
+
+const colorsArr = ['green', 'orange', 'lightblue', 'gray', 'yellow', 'red', 'purple', 'pink', 'violet', 'brown'];
+let count = 1;
+
+const createSpan = (i) => {
+    slider.textContent = '';
+    const span = document.createElement('span');
+    span.classList.add('slide');
+    span.style.backgroundColor = colorsArr[i - 1];
+    slider.appendChild(span);
+}
+
+const addBasicSpan = () => {
+    points.forEach((item) => {
+        item.textContent = 'radio_button_unchecked';
+    })
+}
+
+const nextSlideClick = () => {
+    count++; 
+    if(count > 10){
+        count = 1;
+    }
+    
+    addBasicSpan();
+    points[count - 1].textContent = 'radio_button_checked';
+    createSpan(count);
+}
+
+const backSlideClick = () => {
+    count--;
+    if(count < 1){
+        count = 10;
+    }
+    
+    addBasicSpan();
+    points[count - 1].textContent = 'radio_button_checked';   
+    createSpan(count);
+}
+
+const pointClick = (e) => {
+    addBasicSpan();
+    points[e.target.dataset.id - 1].textContent = 'radio_button_checked';
+    count = e.target.dataset.id;
+    createSpan(count);
+}
+
+const startPageSlider = () => {
+    addBasicSpan();
+    createSpan(count);
+    points[count - 1].textContent = 'radio_button_checked';
+}
+
+nextSlide.addEventListener('click', nextSlideClick);
+backSlide.addEventListener('click', backSlideClick);
+points.forEach((item) => {
+    item.addEventListener('click', pointClick);
+})
+
+startPageSlider();
